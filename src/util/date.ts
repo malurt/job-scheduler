c9922d6dfb124cc59febf6446759fa697378b022
Maria Ribeiro <maria.ribeiro@pagtel.com.br>
2023-01-31 09:46:17 -0300

    feat: generate timestamp from a cron and Brazilian date

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,3 +1,4 @@
+import parser from 'cron-parser';
 import {
   addBusinessDays,
   addDays,
@@ -11,26 +12,49 @@ import {
 } from 'date-fns';
 
 export const convertToTimestamp = (originalExpression: string) => {
-  const currentTimestamp = convertDateToUnix(new Date());
-  const jobExecutionTimestamp = convertDateToUnix(new Date(originalExpression));
+  const BRAZILIAN_DATE_TIME_FORMAT =
+    /^((([012][0-9])|(3[01]))\/([0]{0,1}[1-9]|1[012])\/\d\d\d\d) ([012]{0,1}[0-9]:[0-6][0-9])$/gm;
+  const INTERNATIONAL_DATE_FORMAT =
+    /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]$/gm;
 
-  if (isTimestamp(jobExecutionTimestamp)) {
-    // Valid timestamp (valid date format )
-
-    if (dateIsAfter(currentTimestamp, jobExecutionTimestamp)) {
-      // Invalid Date - The first date is after the second one?
-      throw new Error(
-        'Invalid executionRule date! Execution must happen in a future date'
-      );
+  const finalTimestamp = (() => {
+    // check if its a date
+    if (INTERNATIONAL_DATE_FORMAT.test(originalExpression)) {
+      return convertDateToUnix(new Date(originalExpression));
     }
-    return jobExecutionTimestamp;
-  }
-  // Not an date, possibly a cron or an invalid value
-  return currentTimestamp;
-};
+    if (BRAZILIAN_DATE_TIME_FORMAT.test(originalExpression)) {
+      const dateParts = originalExpression.split(' ');
+      const [day, month, year] = dateParts[0]
+        .split('/')
+        .map((value) => Number(value));
+      const [hours, minutes] = dateParts[1]
+        .split(':')
+        .map((value) => Number(value));
 
-const isTimestamp = (possibleTimestamp: number) =>
-  !Number.isNaN(possibleTimestamp);
+      return convertDateToUnix(new Date(year, month - 1, day, hours, minutes));
+    }
+
+    // check if is a cron
+    const cron = parser.parseString(originalExpression);
+    if (cron.expressions.length >= 1) {
+      return convertDateToUnix(new Date(cron.expressions[0].next().toDate()));
+    }
+
+    throw new Error(
+      'Invalid executionRule format! Try using Brazilian date format (dd/mm/yyyy hh:MM) or cron pattern'
+    );
+  })();
+
+  const currentTimestamp = convertDateToUnix(new Date());
+  if (dateIsAfter(currentTimestamp, finalTimestamp)) {
+    // Invalid Date - The first date is after the second one?
+    throw new Error(
+      'Invalid executionRule date! Execution must happen in a future date'
+    );
+  }
+
+  return finalTimestamp;
+};
 
 export const convertUnixToDate = (unixTime: number): Date => {
   return fromUnixTime(unixTime);
