39ef7504cd427edbda4faff5e3e2cd217865b5ea
Maria Ribeiro <maria.ribeiro@pagtel.com.br>
2023-02-01 08:55:00 -0300

    feat: implement job registration

--- src/infra/db/mssql/scheduler-schema/job-repository.ts
+++ src/infra/db/mssql/scheduler-schema/job-repository.ts
@@ -1,15 +1,27 @@
 import { RegisterJobRepository } from '@/data/protocols/db';
-import { Repository } from '@/infra/db/mssql/util';
+import { Repository, SCHEDULE_JOB_DB } from '@/infra/db/mssql/util';
+import { formateCamelCaseKeysForSnakeCase } from '@badass-team-code/formatted-cases-words';
+
+const {
+  JOB: { JOB },
+} = SCHEDULE_JOB_DB;
 
 export class JobMsSQLRepository
   extends Repository
   implements RegisterJobRepository
 {
-  register(
-    _params: RegisterJobRepository.Params
+  async register(
+    params: RegisterJobRepository.Params
   ): RegisterJobRepository.Result {
-    return new Promise<any>((resolve, _reject) => {
-      resolve(null);
-    });
+    const connection = await this.getConnection();
+
+    const [record] = await connection(JOB.TABLE)
+      .insert(formateCamelCaseKeysForSnakeCase(params))
+      .returning('*');
+
+    return {
+      record,
+      transaction: this.transactionAdapter(connection),
+    };
   }
 }
