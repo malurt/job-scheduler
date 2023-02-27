4ce4fb78f6d04d4c209b6a36cff4b9c3e73184b0
Maria Ribeiro <maria.ribeiro@pagtel.com.br>
2023-02-27 09:51:12 -0300

    fix: correct date and time format

--- src/domain/usecases/schedule-job/register-job.ts
+++ src/domain/usecases/schedule-job/register-job.ts
@@ -11,7 +11,7 @@ export namespace RegisterJob {
   };
   export type Result = Promise<Wrapper<void>>;
   export enum Exceptions {
-    INVALID_EXECUTION_RULE_FORMAT = 'Invalid executionRule format! Try using Brazilian date format (dd/mm/yyyy hh:MM) or cron pattern',
+    INVALID_EXECUTION_RULE_FORMAT = 'Invalid executionRule format! Try using Brazilian date format (yyyy-MM-dd hh:mm) or cron pattern',
     INVALID_EXECUTION_DATE = 'Invalid executionRule date! Execution must happen in a future date',
   }
 }
